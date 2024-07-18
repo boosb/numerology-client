c1934859c17b3a713534eba1754f030360512662
boosb <boosb@mail.ru>
2024-07-18 18:05:40 +0300

    Refactoring. Delete extends because i found out that this is bad practice

--- src/app/components/coins-button/coins-button.component.ts
+++ src/app/components/coins-button/coins-button.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { IUser } from '../../interfaces/user.interface';
+import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ImgService } from '../../services/img.service';
+import { Subscription } from 'rxjs';
+import { IUser } from '../../interfaces/user.interface';
 
 @Component({
   selector: 'app-coins-button',
@@ -13,12 +13,11 @@ import { ImgService } from '../../services/img.service';
   templateUrl: './coins-button.component.html',
   styleUrl: './coins-button.component.scss'
 })
-export class CoinsButtonComponent implements OnInit, OnDestroy {
-  // todo что касается юзера одинаково с main-button, хорошо бы отрефакторить в дальнейшем
+export class CoinsButtonComponent {
   private userSubs: Subscription;
 
   public user: IUser | null = null;
-
+  
   constructor(
     public imgService: ImgService,
     private authService: AuthService,
--- src/app/components/stepper/stepper-views/stepper-view-twelve/stepper-view-twelve.component.ts
+++ src/app/components/stepper/stepper-views/stepper-view-twelve/stepper-view-twelve.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { StepperItemComponent } from '../../stepper-item/stepper-item.component';
-import { StepperViewItemComponent } from '../stepper-view-item/stepper-view-item.component';
 
 @Component({
   selector: 'app-stepper-view-twelve',
@@ -12,6 +11,6 @@ import { StepperViewItemComponent } from '../stepper-view-item/stepper-view-item
     '../../stepper.component.scss'
   ]
 })
-export class StepperViewTwelveComponent extends StepperViewItemComponent {
+export class StepperViewTwelveComponent {
 
 }
