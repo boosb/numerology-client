a39a53cf2b7931b638335a760663b8818b604f3d
Boosb <Boosb@mail.ru>
2024-10-12 18:03:47 +0300

    Commit for testing server

--- src/app/helpers/app.initializer.ts
+++ src/app/helpers/app.initializer.ts
@@ -5,17 +5,5 @@ import { Observable, throwError } from "rxjs";
 export function appInitializer(authService: AuthService) {
 
     // todo почитать. Хорошая ли практика использовать тут промис? Сомневаюсь
-    return () => {
-        return new Promise<void>((resolve, reject) => {
-            authService.refreshToken().pipe(
-                catchError(error => {
-                    console.error('Ошибка при обновлении токена:', error);
-                    // В зависимости от логики приложения можно либо отклонить промис, либо выполнить другие действия
-                    reject(error);
-                    return throwError(() => error); // можно убрать, если не нужно продолжать с ошибкой
-                }),
-                finalize(() => resolve())
-            ).subscribe();
-        });
-    };
+    return () => authService.refreshToken().subscribe();
 }
