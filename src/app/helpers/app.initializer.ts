9d2d4a5e482c9185f17e011088f665e32cc4e54f
Boosb <Boosb@mail.ru>
2024-10-12 18:12:34 +0300

    Commit for testing server

--- src/app/helpers/app.initializer.ts
+++ src/app/helpers/app.initializer.ts
@@ -1,9 +1,12 @@
-import { catchError, finalize } from "rxjs/operators";
 import { AuthService } from "../services/auth.service";
-import { Observable, throwError } from "rxjs";
 
 export function appInitializer(authService: AuthService) {
 
     // todo почитать. Хорошая ли практика использовать тут промис? Сомневаюсь
-    return () => authService.refreshToken().subscribe();
+    return () => new Promise((resolve: any) => {
+        // attempt to refresh token on app start up to auto authenticate
+        authService.refreshToken()
+            .subscribe()
+            .add(resolve);
+    });
 }
